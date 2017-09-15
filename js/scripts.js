a9b7d8298b27a7c45ff4343f3a4e68dd83cd7eb3
sravanthi velaga <svelaga0412@gmail.com>
2017-09-15 14:19:17 -0700

    Add css styles for the body

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
-$(document).ready(function(){
-  var java = 0;
-  var css = 0;
-  var php = 0;
-  var ruby = 0;
+$(function(){
+$("#formone").submit(function(event) {
+  $("#male-rap, #female-rap, #male-pop, #female-pop, #male-classical, #female-classical").hide();
+var ageInput = parseInt($("input#age").val());
+var genderInput = $("select#gender").val();
+var musicInput = $("input:radio[name=music]:checked").val();
 
-  var totalScore = function(score){
-    var score =
-  }
-})
+if (ageInput === false, genderInput === false, musicInput === false) {
+if (genderInput === 'male' && ageInput <=100 && musicInput === 'rap') {
+$("#male-rap").show();
+}
+else if (genderInput === 'female' && ageInput <= 100 && musicInput === 'rap') {
+$("#female-rap").show();
+}
+else if (genderInput === 'male' && ageInput <= 100 && musicInput === 'pop') {
+$("#male-pop").show();
+}
+else if (genderInput === 'female' && ageInput <= 100 && musicInput === 'pop') {
+$("#female-pop").show();
+}
+else if (genderInput === 'male' && ageInput <= 100 && musicInput === 'classical') {
+$("#male-classical").show();
+}
+else if (genderInput === 'female' && ageInput <= 100 && musicInput === 'classical') {
+$("#female-classical").show();
+}
+}
+else {
+  alert("Please enter your details !!!")
+}
+
+
+
+  event.preventDefault();
+
+});
+});
