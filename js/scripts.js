2750369b937e4baf27c8ea8e8f35d52969a5719b
sravanthi velaga <svelaga0412@gmail.com>
2017-09-15 15:04:41 -0700

    Add Back-end logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,29 @@
 $(function(){
 $("#formone").submit(function(event) {
-  $("#8, #9-12, #13-15, #1-19").hide();
-var q1Input = $("input:radio[name=music]:checked").val();
-var q2Input = $("select#gender").val();
-var q3Input = $("select#gender").val();
-var q4Input = $("select#gender").val();
-var q5Input = $("select#gender").val();
 
-if (q1input, q1input, q1input, q1input, q1input) {
-if (q1input === 'male' && ageInput <=100 && musicInput === 'rap') {
-$("#male-rap").show();
-}
-else if (genderInput === 'female' && ageInput <= 100 && musicInput === 'rap') {
-$("#female-rap").show();
-}
-else if (genderInput === 'male' && ageInput <= 100 && musicInput === 'pop') {
-$("#male-pop").show();
-}
-else if (genderInput === 'female' && ageInput <= 100 && musicInput === 'pop') {
-$("#female-pop").show();
-}
-else if (genderInput === 'male' && ageInput <= 100 && musicInput === 'classical') {
-$("#male-classical").show();
-}
-else if (genderInput === 'female' && ageInput <= 100 && musicInput === 'classical') {
-$("#female-classical").show();
-}
-}
-else {
-  alert("Please enter your details !!!")
-}
+    var javaScore = 0;
+    var cSharpScore = 0;
+    var designScore = 0;
+    var tsHighScore = function(score){
+      var highScore = Math.max(javaScore, cSharpScore, designScore);
+      }
+      if(score === highScore && highScore != 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
 
+    var clearScore = function(){
+      var javaScore = 0;
+      var cSharpScore = 0;
+      var designScore = 0;
+    }
 
+    var resetResults = function() {
+      $(#one, #two, #three).addClass("hidden");
+    }
 
   event.preventDefault();
-
 });
 });
