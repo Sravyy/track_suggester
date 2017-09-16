86a24544ecd9b201e2bbc9bfe7cb2033cb66d8f2
sravanthi velaga <svelaga0412@gmail.com>
2017-09-16 01:25:31 -0700

    Add CSS code for classes to hide, make corresponding changes in html/js

--- js/Scripts1.js
+++ js/Scripts1.js
@@ -1,13 +1,8 @@
 $ (function(){
-  //Score calculation
-
-
-//front end
 
       $("#start").click(function(){
         $(".jumbotron").hide();
         $(".questions").fadeIn(1000);
-
       });
 
       $("#survey").submit(function(event){
@@ -16,85 +11,90 @@ $ (function(){
         alert("Hello "+ nameInput + " !");
     //  $(".questions form").submit(function(){
 
-    var javaScore = 0;
-    var cSharpScore = 0;
-    var designScore = 0;
-    var vsHighScore = function(score) {
-    var highScore = Math.max(javaScore, cSharpScore, designScore);
-    if (score === highScore && highScore != 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //questions
-    var q1 = parseInt($("select#q1").val());
-    var q2 = parseInt($("select#q2").val());
-    var q3 = parseInt($("select#q3").val());
-    var q4 = parseInt($("select#q4").val());
-    var q5 = parseInt($("select#q5").val());
-
-      if (q1 === 1){
-        designScore +=1;
-      } else if (q1 === 2) {
-        javaScore +=1;
-        cSharpScore +=1;
+        var javaScore = 0;
+        var cSharpScore = 0;
+        var designScore = 0;
+        var vsHighScore = function(score) {
+        var highScore = Math.max(javaScore, cSharpScore, designScore);
+        if (score === highScore && highScore != 0) {
+          return true;
+        } else {
+          return false;
+        }
       }
 
-      if (q2 === 1){
-        cSharpScore += 1;
-        javaScore += 1;
-      } else if (q2 === 2){
-        designScore += 1;
-      }
+      //questions
+        var q1 = parseInt($("select#q1").val());
+        var q2 = parseInt($("select#q2").val());
+        var q3 = parseInt($("select#q3").val());
+        var q4 = parseInt($("select#q4").val());
+        var q5 = parseInt($("select#q5").val());
 
-      if (q3 === 1){
-        javaScore += 1;
-      } else if (q3 === 2){
-        designScore +=1;
-      } else if (q3 === 3){
-        javaScore += 1;
-        cSharpScore +=1;
-      } else if (q3 === 4){
-        cSharpScore +=1;
-      } else if (q5 === 5){
-        javaScore += 1;
-        cSharpScore += 1;
-        designScore += 1;
-      }
+          if (q1 === 1){
+            designScore +=1;
+          } else if (q1 === 2) {
+            javaScore +=1;
+            cSharpScore +=1;
+          }
 
-      if (q4 === 1){
-        javaScore += 1;
-        cSharpScore += 1;
-      } else if (q4 === 2) {
-        designScore +=1;
-      } else if (q5 === 3){
-        designScore +=1;
-      }
+          if (q2 === 1){
+            cSharpScore += 1;
+            javaScore += 1;
+          } else if (q2 === 2){
+            designScore += 1;
+          }
 
-      if (q5 === 1){
-        designScore +=1;
-      } else if (q5 === 2){
-        javaScore += 1;
-      } else if (q5 === 3){
-        cSharpScore += 1;
-        javaScore += 1;
-      } else if (q5 === 4){
-        javaScore += 1;
-      } else if (q5 === 5){
-        designScore +=1;
-      }
+          if (q3 === 1){
+            javaScore += 1;
+          } else if (q3 === 2){
+            designScore +=1;
+          } else if (q3 === 3){
+            javaScore += 1;
+            cSharpScore +=1;
+          } else if (q3 === 4){
+            cSharpScore +=1;
+          } else if (q5 === 5){
+            javaScore += 1;
+            cSharpScore += 1;
+            designScore += 1;
+          }
 
-    if (vsHighScore(javaScore) === true) {
-    $("#one, #p-l-comic").show();
+          if (q4 === 1){
+            javaScore += 1;
+            cSharpScore += 1;
+          } else if (q4 === 2) {
+            designScore +=1;
+          } else if (q5 === 3){
+            designScore +=1;
+          }
 
-  } else if (vsHighScore(cSharpScore) === true) {
-    $("#two, #p-l-comic").show();
-  } else if (vsHighScore(designScore) === true) {
-    $("#three, #p-l-comic").show();
-    }
-event.preventDefault();
+          if (q5 === 1){
+            designScore +=1;
+          } else if (q5 === 2){
+            javaScore += 1;
+          } else if (q5 === 3){
+            cSharpScore += 1;
+            javaScore += 1;
+          } else if (q5 === 4){
+            javaScore += 1;
+          } else if (q5 === 5){
+            designScore +=1;
+          }
+
+        if (vsHighScore(javaScore) === true) {
+        $("#one, #p-l-comic").addClass("language");
+         event.preventDefault();
+      } else if (vsHighScore(cSharpScore) === true) {
+        $("#two, #p-l-comic").addClass("language");
+         event.preventDefault();
+      } else if (vsHighScore(designScore) === true) {
+        $("#three, #p-l-comic").addClass("language");
+        }
+        event.preventDefault();
+
+        $(".final, #one").show();
+        $("#p-l-comic").fadeIn(2000);
+        $(".questions").hide();
   });
 
 
